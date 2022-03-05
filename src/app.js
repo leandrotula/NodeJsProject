7998b0e75448958d1945001bf69affdc31b73348
Leandro Tula <leandro.tula.expert@opinno.com>
2022-03-05 14:56:34 -0300

    Added ejs partials for icaro course full stack 2022 edition

--- src/app.js
+++ src/app.js
@@ -3,17 +3,9 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-// Declaramos un objeto, que tendra nombre y apellido
-const usuario = {
-    nombre: 'nombre',
-    apellido: 'apellido',
-}
-
 // En este punto, ademas de especificar la vista, pasamos el objeto a nuestra vista
 app.get('/', (req, res) => {
-    res.render('vistas/bienvenido', {
-        usuario: usuario
-    })
+    res.render('home')
 })
 
 
