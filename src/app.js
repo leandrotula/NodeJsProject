e7845e075e412bf060b51b20ad7c316996d0c500
Leandro Tula <leandro.tula.expert@opinno.com>
2022-03-07 19:05:42 -0300

    Added object with render

--- src/app.js
+++ src/app.js
@@ -3,9 +3,20 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-// En este punto, ademas de especificar la vista, pasamos el objeto a nuestra vista
+const usuario = {
+    nombre: 'nombre',
+    apellido: 'apellido',
+}
+
+// Accedemos al empezar la aplicacion, al home.ejs
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/bienvenido', (req, res) => {
+    res.render('vistas/bienvenido', {
+        usuario: usuario
+    })
 })
 
 
